b16e622c9eabbbc2f233e63d98d9bffe28430a91
tokoyoda naoto <ntokoyoda@sevenstars-ltd.com>
2025-08-18 16:34:14 +0900

    remove: existing report warning message as requested by user

--- src/components/DailyReport.tsx
+++ src/components/DailyReport.tsx
@@ -324,25 +324,7 @@ export default function DailyReport() {
         </div>
       </div>
 
-      {/* 既存日報の警告 */}
-      {existingReport.exists && (
-        <div className="mb-6 p-4 bg-yellow-50 border border-yellow-200 rounded-lg">
-          <div className="flex items-center">
-            <svg className="w-5 h-5 text-yellow-600 mr-2" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-            </svg>
-            <div>
-              <span className="text-sm font-medium text-yellow-800">
-                既存の日報が存在します
-              </span>
-              <p className="text-xs text-yellow-600 mt-1">
-                {reportData.date} に {reportData.workerName} の日報が既に存在します。
-                新しい作業項目を追加する場合は、既存の日報に追加されます。
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
+
 
       {/* 作業者履歴表示 */}
       {reportData.workerName && (
